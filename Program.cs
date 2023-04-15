653d465d93849782a6eb1e004cace57a6e543e4e
Mokeddem moussa <mokeddem.moussa@yahoo.com>
2023-04-15 11:18:13 -0800

    creation de code de conflit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace GitConflictDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour !");
+            Console.WriteLine("Bienvenue dans notre application !");
+            Console.WriteLine("Appuyez sur une touche pour quitter.");
+            Console.ReadKey();
+        }
+    }
+}
