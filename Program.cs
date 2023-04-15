981508f176f7d689222cf89fec132e4904c248d5
Mokeddem moussa <mokeddem.moussa@yahoo.com>
2023-04-15 11:23:21 -0800

    ajouter une ligne supplimnetaire

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace GitConflictDemo
         {
             Console.WriteLine("Bonjour !");
             Console.WriteLine("Bienvenue dans notre application !");
+            Console.WriteLine("Ceci est la branche feature !");
             Console.WriteLine("Appuyez sur une touche pour quitter.");
             Console.ReadKey();
         }
